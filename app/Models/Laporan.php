54b37b58e031ae51c39c2d1077413c46348ad966
HilmiSatriaji21 <hilmisatriaji21@gmail.com>
2021-01-29 11:11:36 +0700

    LiveWare Complete

--- app/Models/Laporan.php
+++ app/Models/Laporan.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Laporan extends Model
 {
-    use HasFactory;
+    protected $table = "laporans";
+    protected $fillable = ['id', 'positif', 'sembuh', 'meninggal', 'tanggal', 'id_rw'];
+    public $timestamps = true;
+
+    public function rw()
+    {
+        return $this->belongsTo('App\Models\Rw', 'id_rw');
+    }
+
+    public function laporan()
+    {
+        return $this->hasMany('App\Models\Laporan', 'id_laporan');
+    }
 }
